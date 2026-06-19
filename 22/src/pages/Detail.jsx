1b9a15c7d3c83399a8ccf1ea0df0a66e298527c1
Win.D.Life <ss6112002@naver.com>
2026-06-20 03:42:19 +0900

    22.1

---
+++ 22/src/pages/Detail.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router';
+
+function Detail() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const getMoive = async () => {
+    const json = await (
+      await fetch(`https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters/${id}`)
+    ).json();
+    setMovie(json);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMoive();
+     // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <div className="detail">
+      {loading ? '' : <Link to="/">← 뒤로가기</Link>}
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        movie.data.results.map((movie) => (
+        <div>
+          <h3>{movie.name}</h3>
+          <img src={`${movie.thumbnail.path}.${movie.thumbnail.extension}`} alt='' />
+          <p>{movie.description}</p>
+          <h4><a href={movie.resourceURI} target='_blank' rel="noopener noreferrer">
+            {movie.resourceURI}</a></h4>
+        </div>
+      ))
+    )}
+    </div>
+  );
+}
+
+export default Detail;
---
+++ 22/src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router';
+
+function Home() {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(`https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023`)
+    ).json();
+    setMovies(json);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      {loading ? '' : <h1>Movie Explorer</h1>}
+      <ul>
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : (
+          movies.data.results.map((movie) => (
+            <li key={movie.id}>
+              <Link to={`/movie/${movie.id}`}>
+                <img src={`${movie.thumbnail.path}.${movie.thumbnail.extension}`} alt='' />
+                <h3>{movie.name}</h3>
+                <h4>{movie.resourceURI}</h4>
+              </Link>
+            </li>
+          ))
+        )}
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
